aef74d3666dd95a4265335af45d104779c2ddd71
fuchsvomwalde <jascha.quintern@googlemail.com>
2020-09-22 11:45:55 +0200

    Update project by reading the file system.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # mein-detail-Browser
+
 Ein. Detail Browser zum machen
+
+## Prerequisites
+
+- Install the long term version of node and npm
+
+## How to start the app
+
+1. Load dependencies via `npm install`
+1. Install `nodemon` via `npm install -g nodemon`
+1. Execute `nodemon` to start the app and open `http://localhost:3000/` in your browser
--- app.js
+++ app.js
@@ -1,50 +1,67 @@
-const { text } = require("express");
-const express = require("express");
-const fs = require("fs");
-const { get } = require("http");
+const { text } = require('express')
+const express = require('express')
+const fs = require('fs')
+const path = require('path')
+const { get } = require('http')
 
-const app = express();
+const app = express()
+
+const startDirectory = __dirname
+
 //register vieew engine
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs')
 
 //listen for requests,
-app.listen(3000);
+app.listen(3000)
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
+app.get('/', (req, res) => {
+  fs.readdir(startDirectory, (err, files) => {
+    res.render('index', { dirs: files })
+  })
+})
 
-app.get("/my-website", (req, res) => {
-  res.render("my-website");
-});
-app.get("/node-js", (req, res) => {
-  res.render("node-js");
-});
-app.get("/projeckts", (req, res) => {
-  res.render("projeckts");
-});
-app.get("/test", (req, res) => {
-  res.render("test");
-});
-app.get("/try-again", (req, res) => {
-  res.render("try-again");
-});
+app.use((req, res) => {
+  const newPath = path.join(startDirectory, req.path)
+  console.log(newPath)
+
+  fs.readdir(newPath, (err, files) => {
+    if (err) {
+      res.status(404).render('404')
+    } else {
+      res.render('index', { dirs: files })
+    }
+  })
+})
+
+// app.get('/my-website', (req, res) => {
+//   res.render('my-website')
+// })
+// app.get('/node-js', (req, res) => {
+//   res.render('node-js')
+// })
+// app.get('/projeckts', (req, res) => {
+//   res.render('projeckts')
+// })
+// app.get('/test', (req, res) => {
+//   res.render('test')
+// })
+// app.get('/try-again', (req, res) => {
+//   res.render('try-again')
+// })
 
 /*readStream.on("data", (chunk) => {
   console.log("/...New chunk/n");
   cosnsole.log(chunk);
 });*/
 //register
-app.get("/home", (req, res) => {
-  res.redirect("");
-});
+// app.get('/home', (req, res) => {
+//   res.redirect('')
+// })
 //err
-app.use((req, res) => {
-  res.status(404).render("404");
-});
-let myFileloader = function (ha) {
-  return "myfileloader: " + fs.readFileSync(text);
-};
-app.get("/ha", (req, res) => {
-  res.render("ha");
-});
+
+// let myFileloader = function (ha) {
+//   return 'myfileloader: ' + fs.readFileSync(text)
+// }
+// app.get('/ha', (req, res) => {
+//   res.render('ha')
+// })
