4b48331fdea5b356a8aa0c8f417ffc4308328385
abbas495 <aalsory64@hotmail.com>
2020-09-25 14:50:46 +0200

    Show folder information (size , created , folder number)

--- app.js
+++ app.js
@@ -1,124 +1,107 @@
-const { text } = require('express')
-const express = require('express')
-const fs = require('fs')
-const path = require('path')
-const { get } = require('http')
+// 2st5raj al mlfat ll2st3ml
+// const { text } = require("express");
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const moment = require("moment");
+const { dir } = require("console");
+// const { get } = require("http");
+const app = express();
+const startDirectory = __dirname;
+const stats = fs.statSync(startDirectory);
 
-const app = express()
+// * Render given files as directory HTML page.
 
-const startDirectory = __dirname
+const renderDirectory = (res, parentDir, files, stats) => {
+  // t3rif al dlawal ltl3 al 2r9am
+  //Show the number of files in the folder
+  const options = {
+    parentDir: parentDir,
 
-/**
- * Render given files as directory HTML page.
- */
-const renderDirectory = (res, path, files) => {
-  res.render('index', {
-    parentDir: path,
-    dirs: files,
-    content: '',
-  })
-}
+    dirs: files.map((item) => {
+      const currentPath = path.join(parentDir, item);
+      const isDirectory = fs.statSync(currentPath).isDirectory();
 
-/**
- * Read content of given file path and render as HTML page.
- */
-const renderFile = (res, path, content) => {
-  fs.readFile(path, 'utf8', (err, data) => {
+      return {
+        name: item,
+        entries: isDirectory ? fs.readdirSync(currentPath).length : 0,
+      };
+    }),
+    content: "",
+
+    stats: {
+      nummbier: files.length,
+      createdAt: moment(stats.birthtime).format("DD.MM.YYYY"),
+    },
+  };
+  res.render("index", options);
+};
+
+//* Read content of given file path and render as HTML page.
+
+const renderFile = (res, path, stats) => {
+  fs.readFile(path, "utf8", (err, data) => {
     if (err) {
-      return showError(res, path, err.toString())
+      return showError(res, path, err.toString());
     }
-    res.render('file', {
+    //Show file information
+    console.log(stats);
+    res.render("file", {
       currentDir: path,
       content: data,
-    })
-  })
-}
+      stats: {
+        createdAt: moment(stats.birthtime).format("DD.MM.YYYY"),
+        sizeAt: stats.size,
+      },
+    });
+  });
+};
 
-/**
- * Render given error code as HTML page.
- */
+//* Render given error code as HTML page.
+
 const showError = (res, path, error) => {
-  res.status(404).render('404', { currentDir: path, error: error || '' })
-}
+  res.status(404).render("404", { currentDir: path, error: error || "" });
+};
 
-// const startDirectory2 = __filename
 //register vieew engine
-app.set('view engine', 'ejs')
+
+app.set("view engine", "ejs");
 
 //listen for requests,
-app.listen(3000)
+app.listen(3000);
 
-app.get('/', (req, res) => {
+/*app.get("/", (req, res) => {
   fs.readdir(startDirectory, (err, files) => {
     if (err) {
-      showError(res, startDirectory, err.toString())
+      showError(res, startDirectory, err.toString());
     } else {
-      renderDirectory(res, '', files)
+      renderDirectory(res, "", files, stats);
     }
-  })
-})
-
-// app.use(express.static(startDirectory));
-/*app.get("viwes", function (req, res) {
-  res.status(200).send("User Page");
+  });
 });*/
-
+// alshrt yli 5lana nf3l al code
+//Open files inside folders
 app.use((req, res) => {
-  const newPath = path.join(startDirectory, req.path.replace(/\$SL/g, '/'))
-  const parent = path.relative(__dirname, newPath)
+  const newPath = path.join(startDirectory, req.path.replace(/\$SL/g, "/"));
+  const parent = path.relative(__dirname, newPath);
 
   fs.stat(newPath, (err, stats) => {
     if (err) {
-      showError(res, newPath, err.toString())
+      showError(res, newPath, err.toString());
     } else {
-      // TODO: use stats
-      // console.log(stats)
-
       if (stats.isDirectory()) {
         fs.readdir(newPath, (err, files) => {
           if (err) {
-            showError(res, newPath, err.toString())
+            showError(res, newPath, err.toString());
           } else {
-            renderDirectory(res, parent, files)
+            renderDirectory(res, parent, files, stats);
           }
-        })
+        });
       } else if (stats.isFile()) {
-        renderFile(res, newPath)
+        renderFile(res, newPath, stats);
       } else {
-        showError(res, newPath, err.toString())
+        showError(res, newPath, err.toString());
       }
     }
-  })
-})
-// app.get('/my-website', (req, res) => {
-//   res.render('my-website')
-// })
-// app.get('/node-js', (req, res) => {
-//   res.render('node-js')
-// })
-// app.get('/projeckts', (req, res) => {
-//   res.render('projeckts')
-// })
-// app.get('/test', (req, res) => {
-//   res.render('test')
-// })
-// app.get('/try-again', (req, res) => {
-//   res.render('try-again')
-// })
-
-/*readStream.on("data", (chunk) => {
-  console.log("/...New chunk/n");
-  cosnsole.log(chunk);
-});*/
-//register
-// app.get('/home', (req, res) => {
-//   res.redirect('')
-// })
-//err
-
-// let myFileloader = function (ha) {
-//   return 'myfileloader: ' + fs.readFileSync(text)
-// }
-// app.get('/ha', (req, res) => {
-//   res.render('ha')
-// })
+  });
+});
